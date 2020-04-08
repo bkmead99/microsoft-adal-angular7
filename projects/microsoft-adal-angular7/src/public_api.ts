d14decde30415a3ddaa53e1a37d4bf23687ae7d7
bkmead99 <63377230+bkmead99@users.noreply.github.com>
2020-04-09 09:36:34 +1000

    Uploaded src files

---
+++ projects/microsoft-adal-angular7/src/public_api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of microsoft-adal-angular7
+ */
+
+export * from './lib/authentication-guard';
+export * from './lib/ms-adal-angular6.module';
+export * from './lib/ms-adal-angular6.service';
